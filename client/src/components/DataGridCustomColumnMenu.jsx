cdd6f53b0fb39d84b69942ac8f6f8daf452e883d
Piyush Gupta <guptapiyush315@gmail.com>
2023-05-20 00:52:08 +0530

    .

--- client/src/components/DataGridCustomColumnMenu.jsx
+++ client/src/components/DataGridCustomColumnMenu.jsx
@@ -1,6 +1,6 @@
 import {
     GridColumnMenuContainer,
-    GridFilterMenuItem,
+    // GridFilterMenuItem,
     HideGridColMenuItem,
   } from "@mui/x-data-grid";
   
@@ -12,7 +12,8 @@ import {
         currentColumn={currentColumn}
         open={open}
       >
-        <GridFilterMenuItem onClick={hideMenu} column={currentColumn} />
+        <HideGridColMenuItem onClick={hideMenu} column={currentColumn} />
+        {/* <GridFilterMenuItem onClick={hideMenu} column={currentColumn} /> */}
         <HideGridColMenuItem onClick={hideMenu} column={currentColumn} />
       </GridColumnMenuContainer>
     );
